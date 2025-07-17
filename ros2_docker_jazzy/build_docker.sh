a1a094fd41f28d5063bbdf18cc959963e5f46f66
Nitz719 <42895411+Nitz719@users.noreply.github.com>
2025-07-17 11:02:34 +0530

    Update build_docker.sh
    Added the updated of the Turtlebot4

--- ros2_docker_jazzy/build_docker.sh
+++ ros2_docker_jazzy/build_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="ros2-jazzy-dev"
+IMAGE_NAME="ros2-jazzy-turtlebot4"
+TAG="latest"
 
-echo "Building Docker image: $IMAGE_NAME"
-docker build -t $IMAGE_NAME .
+echo "Building Docker image: ${IMAGE_NAME}:${TAG}"
+docker build -t "${IMAGE_NAME}:${TAG}" .
