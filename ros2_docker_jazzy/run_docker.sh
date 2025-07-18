5aa7146465a9ca5a6283c53e4db69114ae98e1a4
Nitz719 <42895411+Nitz719@users.noreply.github.com>
2025-07-18 23:40:08 +0530

    Update run_docker.sh
    Webot Simulator

--- ros2_docker_jazzy/run_docker.sh
+++ ros2_docker_jazzy/run_docker.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -e
 
-IMAGE_NAME="ros2-jazzy-turtlebot4:latest"
-CONTAINER_NAME="ros2_jazzy_turtlebot4"
-
-WORKSPACE_DIR="$(pwd)/ros2_ws"
-mkdir -p "$WORKSPACE_DIR"
+IMAGE_NAME="ros2-jazzy-dev"
+CONTAINER_NAME="ros2_jazzy_container"
 
 echo "Launching TurtleBot 4 simulation in container: $CONTAINER_NAME"
+
+# Enable X11 access for Docker
+xhost +local:docker
+
+# Run the Docker container with volume and interactive shell
+echo "Running container: $CONTAINER_NAME"
 docker run -it --rm \
-  --name "$CONTAINER_NAME" \
-  --net=host \
-  --privileged \
-  -e DISPLAY="$DISPLAY" \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v "$WORKSPACE_DIR":/ros2_ws \
-  -w /ros2_ws \
-  "$IMAGE_NAME"
+    --name $CONTAINER_NAME \
+    --net=host \
+    --privileged \
+    -e DISPLAY="$DISPLAY" \
+    -e QT_X11_NO_MITSHM=1 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $(pwd)/ros2_ws:/ros2_ws \
+    $IMAGE_NAME
